14f75e709bf0dd1151cb677abcd7b7190f66e6c2
jbossios <bossjonad@gmail.com>
2022-06-22 14:27:54 +0200

    Add option file used for NN v96
    Minor updates to scripts used for pipelines

--- run_signal_training.py
+++ run_signal_training.py
@@ -4,9 +4,9 @@ import sys
 if __name__ == '__main__':
   """ Submit SPANet training or evaluation jobs """
   # Settings
-  run_type = 'predict' # options: train, predict
+  run_type = 'train' # options: train, predict
   path = '/eos/atlas/atlascerngroupdisk/phys-susy/RPV_mutlijets_ANA-SUSY-2019-24/spanet_jona/SPANET_OptionFiles/'
-  versions = [i for i in range(72, 92)]
+  versions = [i for i in range(96, 97)]  # network settings
   # create dummy output folder to force the output version that I want
   if run_type == 'train' and not os.path.exists(f'spanet_output/version_{versions[0]-1}'):
     os.makedirs(f'spanet_output/version_{versions[0]-1}')
@@ -20,9 +20,9 @@ if __name__ == '__main__':
         with open(f'options_files/{infile}', 'r') as opt:
           lines = opt.readlines()
           for line in lines:
-            if 'testing' in line and 'h5' in line:
+            if 'training' in line and 'h5' in line:
               h5_file = line.split(':')[1].replace(',' ,'').replace('"', '').replace('\n', '')
-        command = f'python3 predict.py ./spanet_output/version_{version} ./signal_full_v{version}_output.h5 -tf {h5_file} --gpu > Log_v{version}_{run_type} 2>&1'
+        command = f'python3 predict.py ./spanet_output/version_{version} ./signal_full_v{version}_output.h5 -tf{h5_file} --gpu > Log_v{version}_{run_type} 2>&1'
       else:
         print(F'ERROR: {run_type} not implemented yet, exiting')
         sys.exit(1)
--- submit_pipelines.py
+++ submit_pipelines.py
@@ -1,10 +1,10 @@
 import kfp
 import sys
 
-def submit_pipeline(date, spanet_version, pipeline_id, path, experiment_id = 'dijetseval11032022'):
+def submit_pipeline(sample, date, spanet_version, pipeline_id, path, experiment_id = 'dijetseval11032022'):
   client = kfp.Client()
-  pipeline_name = f'spanet-dijets-eval-{date}-{spanet_version}-id{pipeline_id}'
-  pipeline_file = f'{path}spanet_dijets_eval_{date}_{spanet_version}_{pipeline_id}.yaml'
+  pipeline_name = f'spanet-{sample}-eval-{date}-{spanet_version}-id{pipeline_id}'
+  pipeline_file = f'{path}spanet_{sample}_eval_{date}_{spanet_version}_{pipeline_id}.yaml'
   print(f'INFO: Submitting {pipeline_file}...')
   if client.get_pipeline_id(pipeline_name): # pipeline already exists
     print(f'ERROR: Pipeline already exists. Delete pipeline with the following:')
@@ -17,11 +17,15 @@ def submit_pipeline(date, spanet_version, pipeline_id, path, experiment_id = 'di
   run = client.run_pipeline(exp.id, pipeline_name, pipeline_file)
 
 if __name__ == '__main__':
-  date = '11032022'
-  n_yaml_files_per_version = 4
-  versions = [f'v{i}' for i in range(74, 92)]
+  date = '21062022'
+  sample = 'signal'  # options: dijets, signal
+  n_yaml_files_per_version = {
+    'signal': 5,
+    'dijets': 3,
+  }[sample]
+  versions = [f'v{i}' for i in range(96, 97)]
   path_to_yaml_files = '/eos/atlas/atlascerngroupdisk/phys-susy/RPV_mutlijets_ANA-SUSY-2019-24/spanet_jona/SPANET_Pipelines_yaml_files/'
   for version in versions:
     for i in range(n_yaml_files_per_version):
-      submit_pipeline(date, version, i, path_to_yaml_files)
+      submit_pipeline(sample, date, version, i, path_to_yaml_files)
   print('>>> ALL DONE <<<')

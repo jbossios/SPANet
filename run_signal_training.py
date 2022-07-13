fae3820f38e89e748c76bc30bf14defb42935929
jbossios <bossjonad@gmail.com>
2022-07-13 14:53:49 +0200

    Update run_signal_training.py to use new scripts
    new scripts: train_test.py and evaluate.py

--- run_signal_training.py
+++ run_signal_training.py
@@ -6,27 +6,23 @@ if __name__ == '__main__':
   # Settings
   run_type = 'train' # options: train, predict
   path = '/eos/atlas/atlascerngroupdisk/phys-susy/RPV_mutlijets_ANA-SUSY-2019-24/spanet_jona/SPANET_OptionFiles/'
-  versions = [i for i in range(96, 97)]  # network settings
+  training_file = '/eos/atlas/atlascerngroupdisk/phys-susy/RPV_mutlijets_ANA-SUSY-2019-24/ntuples/tag/input/mc16e/GG_rpv/PROD0/h5/v0/user.abadea.GGrpv2x3ALL_minJetPt50_minNjets6_maxNjets8_RDR_dr_v0.h5'
+  root_file = '/eos/atlas/atlascerngroupdisk/phys-susy/RPV_mutlijets_ANA-SUSY-2019-24/ntuples/tag/input/mc16e/GG_rpv/PROD0/user.abadea.mc16_13TeV.504518.MGPy8EG_A14NNPDF23LO_GG_rpv_UDB_1400_squarks.r10724_PROD0_trees.root/user.abadea.504518.e8258_e7400_s3126_r10724_r10726_p5083.29059700._000049.trees.root'
+  versions = [i for i in range(97, 98)]  # network settings
   # create dummy output folder to force the output version that I want
   if run_type == 'train' and not os.path.exists(f'spanet_output/version_{versions[0]-1}'):
     os.makedirs(f'spanet_output/version_{versions[0]-1}')
   commands = []
   for version in versions:
-    inpath = f'{path}v{version}/'
-    for infile in os.listdir(inpath):
-      if run_type == 'train':
-        command = f'python3 train.py -of options_files/{infile} --random_seed 1 --gpus 1 > Log_v{version}_{run_type} 2>&1'
-      elif run_type == 'predict':
-        with open(f'options_files/{infile}', 'r') as opt:
-          lines = opt.readlines()
-          for line in lines:
-            if 'training' in line and 'h5' in line:
-              h5_file = line.split(':')[1].replace(',' ,'').replace('"', '').replace('\n', '')
-        command = f'python3 predict.py ./spanet_output/version_{version} ./signal_full_v{version}_output.h5 -tf{h5_file} --gpu > Log_v{version}_{run_type} 2>&1'
-      else:
-        print(F'ERROR: {run_type} not implemented yet, exiting')
-        sys.exit(1)
-      commands.append(command)
+    opt_infile = f'signal_v{version}_partial.json'
+    if run_type == 'train':
+      command = f'python3 train_test.py -of options_files/{opt_infile} -ef event_files/signal.ini -tf {training_file} --random_seed 1 --gpus 1 > Log_v{version}_{run_type} 2>&1'
+    elif run_type == 'predict':
+      command = f'python3 evaluate.py -l ./spanet_output/version_{version} -v {version} -i{root_file} --gpu > Log_v{version}_{run_type} 2>&1'
+    else:
+      print(F'ERROR: {run_type} not implemented yet, exiting')
+      sys.exit(1)
+    commands.append(command)
   command = ' && '.join(commands)
   command += ' &'
   print(command)
